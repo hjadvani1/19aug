6734f9a23f39b4d270df99be76dc9393e80ce8a3
hjadvai1 <jadvanihiren12@gmail.com>
2021-09-08 18:33:50 +0530

    8sep works

--- app.js
+++ app.js
@@ -1,53 +1,53 @@
-//rest parameter
-function greet(greeting:string,...names:string[])
-{
-    console.log(`${greeting} "${names.join(",")}!`);
-}
-// greet("Ohayo", "Naruto","Uzumaki")
-// interface
-interface charcter
-{
-    (name: string, char: string, ep:number): void;
-};
-function series1(name:string, char:string, ep :number):void { 
-    console.log(`${char} is from ${name} and its have ${ep} episodes`)
-}
-function series2(name:string, char:string, ep :number):void { 
-    console.log(`${char} is from ${name} and its have ${ep} episodes`)
-}
-let show: charcter = series1;
-show("Naruto","Narto",720); 
-show = series2;
-show("Fairy Tail","Natsu",328);
-//structural type system
-interface category
-{
-    name:string,
-    type : string,
-    seasons : number,
-}
-function showDetails( sd: category)
-{
-    console.log(`${sd.name} is ${sd.type} type of series and its have ${sd.seasons} seasons`);
-}
-const category ={name:"FRIENDS",type:"Sitcom",seasons:10}
-showDetails(category)
-function Hello(person:string, date:Date):void {
-    console.log("Hello " + person + ", today is " + date.toDateString() + "!");
-}
-Hello("chandler", new Date());
-//optional property 
-function printName(obj: { first: string; last?: string }) {
-    if (obj.last !== undefined) {
-      console.log(obj.first.toUpperCase()+" " + obj.last.toUpperCase());
-    //   console.log("hello");
-    }
-   else{
-        console.log(`hey ${obj.first} please enter your last name`);
-   }
-  }
-//   printName({first:"chandler"})
-  printName({first:"chandler", last:"bing"})
+// //rest parameter
+// function greet(greeting:string,...names:string[])
+// {
+//     console.log(`${greeting} "${names.join(",")}!`);
+// }
+// // greet("Ohayo", "Naruto","Uzumaki")
+// // interface
+// interface charcter
+// {
+//     (name: string, char: string, ep:number): void;
+// };
+// function series1(name:string, char:string, ep :number):void { 
+//     console.log(`${char} is from ${name} and its have ${ep} episodes`)
+// }
+// function series2(name:string, char:string, ep :number):void { 
+//     console.log(`${char} is from ${name} and its have ${ep} episodes`)
+// }
+// let show: charcter = series1;
+// show("Naruto","Narto",720); 
+// show = series2;
+// show("Fairy Tail","Natsu",328);
+// //structural type system
+// interface category
+// {
+//     name:string,
+//     type : string,
+//     seasons : number,
+// }
+// function showDetails( sd: category)
+// {
+//     console.log(`${sd.name} is ${sd.type} type of series and its have ${sd.seasons} seasons`);
+// }
+// const category ={name:"FRIENDS",type:"Sitcom",seasons:10}
+// showDetails(category)
+// function Hello(person:string, date:Date):void {
+//     console.log("Hello " + person + ", today is " + date.toDateString() + "!");
+// }
+// Hello("chandler", new Date());
+// //optional property 
+// function printName(obj: { first: string; last?: string }) {
+//     if (obj.last !== undefined) {
+//       console.log(obj.first.toUpperCase()+" " + obj.last.toUpperCase());
+//     //   console.log("hello");
+//     }
+//    else{
+//         console.log(`hey ${obj.first} please enter your last name`);
+//    }
+//   }
+// //   printName({first:"chandler"})
+//   printName({first:"chandler", last:"bing"})
 //callback function
 function weeb(name, favanime) {
     console.log("Hey i am " + name);
